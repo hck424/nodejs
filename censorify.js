03ce2ae51911a9b96d8d9bd83de4c1c1b62527df
hck424 <sangchang86@hotmail.com>
2016-10-03 21:49:56 +0900

    Initial commit

---
+++ censorify.js
@@ -0,0 +1,24 @@
+/**
+ * Created by hakcheol on 2016. 10. 3..
+ */
+var censoredWords = ["sad", "bad","mad"];
+var customCensoredWords = [];
+
+function censor(inStr) {
+    for (idx in censoredWords) {
+        inStr = inStr.replace(censoredWords[idx], "****");
+    }
+    for (idx in customCensoredWords){
+        inStr = inStr.replace(customCensoredWords[idx],"****");
+    }
+    return inStr;
+}
+function addCensoredWord(word) {
+    customCensoredWords.push(word);
+}
+function  getCensoredWords() {
+    return censoredWords.concat(customCensoredWords);
+}
+exports.censor = censor;
+exports.addCensordWord = addCensoredWord;
+exports.getCensoredWords.getCensoredWords;
